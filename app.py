ca903d4f5e7f25872a0e99a1f358d6abb1a76e59
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2024-04-08 19:03:35 +0200

    updates

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+conn = psyc
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+@app.route('/auth/login', methods=['POST'])
+def login():
+    return render_template('auth/auth.login.html')
+
+
+@app.route('/auth/register', methods=['POST'])
+def register():
+    return render_template('auth/auth.signup.html')
+
 
 if __name__ == '__main__':
     app.run()
